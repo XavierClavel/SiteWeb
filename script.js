6998bd1ec62500a3a0adbd172df1ed08562c7e99
Xavier Clavel <xrclavel@gmail.com>
2023-01-15 13:55:20 +0100

    responsive design

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ var mini = true;
 function toggleSidebar() {
     console.log("opening sidebar");
     document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
+    //document.getElementById("main").style.marginRight = "250px";
 
 }
 
 function exitSidebar() {
     console.log("closing sidebar");
     document.getElementById("sidebar").style.width = "85px";
-    document.getElementById("main").style.marginRight = "85px";
+    //document.getElementById("main").style.marginRight = "85px";
 }
 
 function alertLog() {
